551aed3783bb9a5b4204e5240b23c9e5158db7f6
Amrik97 <121186386+Amrik97@users.noreply.github.com>
2023-05-02 18:08:44 +0600

    Update tasks_1.java

--- tasks_1.java
+++ tasks_1.java
@@ -5,7 +5,7 @@ import java.util.Random;
 //
 // Пусть дан произвольный список целых чисел, удалить из него чётные числа
 //
-public class task1 {
+public class task_1 {
     public static void main(String[] args) {
         var list = GetRandomList(10, 0, 10);
         System.out.println(list.toString());
